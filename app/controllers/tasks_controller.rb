9628845a779baf98529b215108ac10d581f58436
kiyoshi.chieda <kiyoshi0907@icloud.com>
2020-05-28 04:31:35 +0000

    add user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,36 +1,40 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-      @tasks = Task.all.page(params[:page]).per(5)
-  end
-
-  def show
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
   def new
-      @task = Task.new
+    @task = Task.new
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
   end
 
   def create
-      @task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
       
       if @task.save
           flash[:success] = 'タスクを作成しました'
-          redirect_to @task
+          redirect_to root_url
       else
+          @tasks = current_user.tasks.order(id: :desc).page(params[:page])
           flash.now[:danger] = 'タスクを作成できませんでした'
           render :new
       end
   end
 
-  def edit
-  end
-
   def update
       if @task.update(task_params)
           flash[:success] = 'タスクを更新しました'
-          redirect_to @task
+          redirect_to root_url
       else
           flash.now[:danger] = 'タスクを更新できませんでした'
           render :edit
@@ -41,17 +45,25 @@ class TasksController < ApplicationController
       @task.destroy
       
       flash[:success] = 'タスクを削除しました'
-      redirect_to tasks_url
+      redirect_back(fallback_location: root_path)
   end
 
   private
   
-  def set_task
-    @task = Task.find(params[:id])
-  end  
-
   def task_params
     params.require(:task).permit(:content, :status)
-  end    
-    
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
